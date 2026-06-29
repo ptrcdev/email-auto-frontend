6a348a692b459dca6a4b9554cf8b2859f52bd8ab
Patricia Almeida <patricia.almeida.dev@gmail.com>
2026-06-29 16:16:53 +0100

    changes

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -5,7 +5,7 @@
       <p class="login-subtitle">Sign in to continue</p>
 
       <div class="button-group">
-        <a :href="`${process.env.VUE_APP_API_URL}/auth/google`" class="btn btn-google">
+        <a :href="`${import.meta.env.VITE_API_URL}/auth/google`" class="btn btn-google">
           <svg class="btn-icon" viewBox="0 0 24 24" width="20" height="20">
             <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z" fill="#4285F4"/>
             <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
@@ -15,7 +15,7 @@
           Sign in with Google
         </a>
 
-        <a :href="`${process.env.VUE_APP_API_URL}/auth/microsoft`" class="btn btn-outlook">
+        <a :href="`${import.meta.env.VITE_API_URL}/auth/microsoft`" class="btn btn-outlook">
           <svg class="btn-icon" viewBox="0 0 24 24" width="20" height="20">
             <path d="M21.3 3H2.7C1.76 3 1 3.76 1 4.7v14.6c0 .94.76 1.7 1.7 1.7h18.6c.94 0 1.7-.76 1.7-1.7V4.7c0-.94-.76-1.7-1.7-1.7z" fill="#0078D4"/>
             <path d="M21.3 3l-9.3 7.5L2.7 3" fill="#0058A3"/>
